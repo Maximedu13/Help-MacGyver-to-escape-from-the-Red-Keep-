d61636028159fb78c1ddacc205f747aaef767f67
Maximedu13 <maxime.j.pro@free.fr>
2018-11-25 14:31:27 +0100

    requirements.txt added

--- home.py
+++ home.py
@@ -1,9 +1,12 @@
 # -*- coding: Utf-8 -*
 """It's the homepage and the game launch file."""
-import pygame
 import sys
-from constants import *
-from pygame.locals import *
+import pygame
+import pygame.locals as GAME_GLOBALS
+import pygame.event as GAME_EVENTS
+from constants import SCREENSIZE_MENU, GAME_TITLE, GARDIAN_IMG, \
+INSTRUCTIONS_HOME_1, INSTRUCTIONS_HOME_2, INSTRUCTIONS_HOME_3, WHITECOLOR, \
+BG_IMAGE, FONT_BG, START_SOUND
 from main import loadGame
 
 class Menu():
@@ -12,23 +15,23 @@ class Menu():
     def __init__(self):
         #INITIALISATION
         pygame.init()
-        self.WINDOW = pygame.display.set_mode((SCREENSIZE_MENU), RESIZABLE)
+        self.window = pygame.display.set_mode((SCREENSIZE_MENU), GAME_GLOBALS.RESIZABLE)
         pygame.display.set_caption(GAME_TITLE)
         #BACKGROUNDS LOADING AND STICKING
-        self.HOME = pygame.image.load(BG_IMAGE).convert()
+        self.home = pygame.image.load(BG_IMAGE).convert()
         pygame.mixer.music.load(START_SOUND)
         pygame.mixer.music.play()
-        self.GARDIAN = pygame.image.load(GARDIAN_IMG).convert_alpha()
-        pygame.display.set_icon(self.GARDIAN)
-        self.FONT = pygame.font.Font(FONT_BG, 20)
-        self.TEXT_1 = self.FONT.render(INSTRUCTIONS_HOME_1, 1, (0, 0, 0))
-        self.TEXT_2 = self.FONT.render(INSTRUCTIONS_HOME_2, 1, (0, 0, 0))
-        self.TEXT_3 = self.FONT.render(INSTRUCTIONS_HOME_3, 1, (0, 0, 0))
-        self.WINDOW.fill(WHITECOLOR)
-        self.WINDOW.blit(self.HOME, (0, 0))
-        self.WINDOW.blit(self.TEXT_1, (100, 550))
-        self.WINDOW.blit(self.TEXT_2, (100, 600))
-        self.WINDOW.blit(self.TEXT_3, (100, 650))
+        self.gardian = pygame.image.load(GARDIAN_IMG).convert_alpha()
+        pygame.display.set_icon(self.gardian)
+        self.font = pygame.font.Font(FONT_BG, 20)
+        self.text_1 = self.font.render(INSTRUCTIONS_HOME_1, 1, (0, 0, 0))
+        self.text_2 = self.font.render(INSTRUCTIONS_HOME_2, 1, (0, 0, 0))
+        self.text_3 = self.font.render(INSTRUCTIONS_HOME_3, 1, (0, 0, 0))
+        self.window.fill(WHITECOLOR)
+        self.window.blit(self.home, (0, 0))
+        self.window.blit(self.text_1, (100, 550))
+        self.window.blit(self.text_2, (100, 600))
+        self.window.blit(self.text_3, (100, 650))
         pygame.display.flip()
 
     def render(self):
@@ -36,18 +39,18 @@ class Menu():
         while True:
             #INITIALISATION
             pygame.init()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in GAME_EVENTS.get():
+                if event.type == GAME_GLOBALS.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.KEYDOWN:
+                if event.type == GAME_GLOBALS.KEYDOWN:
                     if event.key == pygame.K_ESCAPE or event.key == pygame.K_F2:
                         pygame.quit()
                         sys.exit()
-                    if event.key == pygame.K_F1:
+                    if event.key == GAME_GLOBALS.K_F1:
                         main = loadGame()
                         main.run()
-                    if event.key == pygame.K_F3:
+                    if event.key == GAME_GLOBALS.K_F3:
                         pygame.mixer.music.stop()
         pygame.quit()
 
